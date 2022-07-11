9213cc41d67c133e63db1a5dbe55e14e829d8e83
Gernot Steinegger <gernot.steinegger@roomle.com>
2022-07-11 09:02:55 +0200

    fear: Roomle script analyzer noed.js project

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
+# Roomle static script analyzer
 
+## Build
+
+Install:
+
+```lang-none
+npm i
+````
+
+Build:
+
+```lang-none
+npm run build
+```
+
+## Run
+
+Run test:
+
+```lang-none
+npm run start:test
+```
+
+Analyze Component JSON
+
+```lang-none
+npm run start <my_path/my_component.json>
+```
---
+++ src/tools/scriptAnalyzerTest.ts
@@ -0,0 +1,37 @@
+import ToolsCoreInterface from '../interface/toolsCore'
+
+const loadToolsCore = async () => {
+    // @ts-ignore
+    const ToolsCoreCore = await require("../../node_modules/roomle-core-hsc/wasm/RoomleToolsCore.js");    
+    const toolsCoreModule = await ToolsCoreCore();
+    toolsCoreModule.setContext({
+        isReady: () => {},
+        throw: (what : string) => console.log("exception: " + what),
+        log: (message : string) => console.log(message),
+    })
+    let toolsCore: ToolsCoreInterface = new toolsCoreModule.ToolsCore()
+    return toolsCore;
+}
+
+const componentDefinitionNoError: string = `
+{
+    "id": "error_test",
+    "geometry": "AddPlainCube(Vector3f{100, 100, 100}); SetObjSurface('isdt:yellow');"
+}`
+
+const componentDefinitionWithError: string = `
+{
+    "id": "error_test",
+    "geometry": "this is an error"
+}`
+
+const analyzeTestComponents = async () => {
+    const toolsCore: ToolsCoreInterface = await loadToolsCore();
+    console.log("Analyze component with no error\n")
+    toolsCore.analyzeComponent(componentDefinitionNoError);
+    console.log("\nAnalyze component with 1 error\n")
+    toolsCore.analyzeComponent(componentDefinitionWithError);
+    console.log("\n")
+}
+
+analyzeTestComponents()
